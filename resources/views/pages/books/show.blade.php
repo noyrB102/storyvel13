82d721e9fc2b40125416fd6287e2da6c5b2584e3
Bryon Swanson <bswanson@outlook.com>
2026-08-05 16:05:53 -0600

    ui: reword and wrap back link text to two lines

--- resources/views/pages/books/show.blade.php
+++ resources/views/pages/books/show.blade.php
@@ -17,7 +17,7 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="size-4" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
                 </svg>
-                My Stories
+                Back to Home page<br>(My Stories)
             </a>
 
             <div class="flex flex-col items-end gap-2">
